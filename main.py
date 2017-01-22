fef981a4b2e81c1ff0d064b98d8c953882ca0047
sk-yoh <guanqiay@andrew.cmu.edu>
2017-01-22 12:56:14 -0500

    add bootstrapframe

--- main.py
+++ main.py
@@ -8,7 +8,8 @@ JINJA_ENVIRONMENT = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
     extensions=['jinja2.ext.autoescape'],
     autoescape=True)
-    
+
+# Import the Flask Framework
 from flask import Flask
 app = Flask(__name__)
 app.config['DEBUG'] = True
@@ -21,8 +22,18 @@ app.config['DEBUG'] = True
 def hello():
     template = JINJA_ENVIRONMENT.get_template('templates/index.html')
     return template.render()
+    
+@app.route('/about')
+def about():
+    template = JINJA_ENVIRONMENT.get_template('templates/about.html')
+    return template.render()
 
 @app.errorhandler(404)
 def page_not_found(e):
     """Return a custom 404 error."""
     return 'Sorry, nothing at this URL.', 404
+    
+@app.errorhandler(500)
+def application_error(e):
+    """Return a custom 500 error."""
+    return 'Sorry, unexpected error: {}'.format(e), 500
